a77dfd8f18861c6e4a06a043e00acbd034a95fe2
hayden.rear <hayden.rear@gmail.com>
2025-05-15 16:27:53 -0400

    move for code complete

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aisuite',
+    version='1.0.0',
+    description='Python library',
+    packages=find_packages('src'),  # Set the 'src' directory as the base for package discovery
+    package_dir={'': 'src'},  # Map the root package to the 'src' directory
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
